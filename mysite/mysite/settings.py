aa8bcf0cbb369d71f424bd7ad7382cdeb75b7600
DozingWolf <43899290+DozingWolf@users.noreply.github.com>
2018-10-16 14:00:54 +0800

    20181016

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,10 +74,29 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+#model of Oracle
+# DATABASE_ENGINE = 'oracle'
+# DATABASE_NAME = 'XE'
+# DATABASE_USER = 'hr'
+# DATABASE_PASSWORD = 'hr'
+# DATABASE_HOST = 'localhost'
+# DATABASE_PORT = '1521'
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '',
+        'USER':'',
+        'PASSWORD':'',
+        'HOST':'',
+        'PORT':'',
     }
 }
 
